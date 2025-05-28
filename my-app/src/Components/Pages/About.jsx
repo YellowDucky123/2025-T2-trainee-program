307ad394f56295bda78c5a66575f06734f7dbcb8
kelvin <kelvinjyoga@gmail.com>
2025-05-28 13:36:30 +1000

    m

--- my-app/src/Components/Pages/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const About = () => {
-    return <div>
-        <h1>My name is Kelvin and I am a 2nd Year Computer Science degree</h1>
-    </div>
-}
--- my-app/src/Components/Pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export const Home = () => {
-  return <div>
-    <h1>This is my Profile web, with "opposites attract" theme</h1>
-    <h1>Blue, green, and red are opposites :))</h1>
-  </div>;
-};
