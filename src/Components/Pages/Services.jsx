83e1b151521bc51b98d4d464ba49247a18d99052
kelvin <kelvinjyoga@gmail.com>
2025-05-28 13:35:30 +1000

    message

---
+++ src/Components/Pages/Services.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Services = () => {
+    return <div>
+        <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae fugit recusandae tenetur asperiores quos ad molestias soluta voluptatem rem, dignissimos quod officiis, sit odio deserunt magnam mollitia magni autem unde sed aliquid excepturi minima fugiat accusamus sequi! Iure dicta cumque saepe, sapiente facere amet. Vero iusto, officiis dolores harum deserunt reprehenderit ipsum commodi voluptatum cumque aspernatur doloremque nihil, eligendi adipisci eveniet ipsa beatae! Eligendi earum adipisci quibusdam maxime tenetur natus suscipit neque qui ea eos? Dicta nam eum quibusdam aliquid sequi laborum, quam earum non molestiae eligendi! Facere nostrum, ex soluta molestiae repellendus illum voluptatum quo optio maxime voluptates? Quos?
+        </div>
+        <br/>
+        <div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus quas tempore harum, corporis repellat ducimus voluptas non deleniti culpa placeat consequatur veritatis aperiam eaque ex sequi amet eum, fugit fugiat quidem quis similique dolores quo, quos molestiae? Explicabo nulla atque vero laboriosam, ratione pariatur architecto exercitationem ab adipisci recusandae in libero officiis, itaque voluptatibus dolorem excepturi inventore asperiores cum quia temporibus? Sed adipisci laborum ullam animi accusantium, reprehenderit sunt itaque, recusandae nulla sint laboriosam natus maiores eum pariatur libero eveniet voluptates dicta expedita quod commodi? Perspiciatis aliquam earum cum hic quia facilis aspernatur laborum quaerat facere, eaque pariatur illum nostrum?
+        </div>
+    </div>
+}
